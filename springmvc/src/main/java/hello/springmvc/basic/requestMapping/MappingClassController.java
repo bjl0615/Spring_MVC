26f0aa1ad575d118cd0836c874fd41f72f1548a6
bjl0615 <bjl0615@gmail.com>
2023-12-07 00:43:23 +0900

    Mapping Controller

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingClassController.java
@@ -0,0 +1,43 @@
+package hello.springmvc.basic.requestMapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+    @GetMapping
+    public String users() {
+        return "get users";
+    }
+    /**
+     * POST /mapping/users
+     */
+    @PostMapping
+    public String addUser() {
+        return "post user";
+    }
+    /**
+     * GET /mapping/users/{userId}
+     */
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable("userId") String userId) {
+        return "get userId=" + userId;
+    }
+    /**
+     * PATCH /mapping/users/{userId}
+     */
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable("userId") String userId) {
+        return "update userId=" + userId;
+    }
+    /**
+     * DELETE /mapping/users/{userId}
+     */
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable("userId") String userId) {
+        return "delete userId=" + userId;
+    }
+
+
+
+}
